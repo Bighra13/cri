bb152753d5d1df3ab220cb30b78201c403ba55bc
Bilal Hassan <bilal.hassan7071@gmail.com>
2024-05-11 21:22:06 +0500

    Update ptv2.sh

--- ptv2.sh
+++ ptv2.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-STREAM_URL = "http://starshare.live:8080/PTVSports/SportsPTV/61674"
-M3U8_FILE = "live_channel.m3u8"
-TS_FILE = "ptv.m3u8"
-
+STREAM_URL="http://starshare.live:8080/PTVSports/SportsPTV/61674"
+M3U8_FILE="live_channel.m3u8"
+TS_FILE="ptv.m3u8"
 
 # Trap Ctrl+C to gracefully exit the loop
 trap 'echo "Exiting..."; exit' INT
 
 # Infinite loop for continuous execution
 while true; do
-rm -f "$TS_FILE"
-ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "../$TS_FILE"
-mv "../$M3U8_FILE.tmp" "../$M3U8_FILE"
+    rm -f "$TS_FILE"
+    ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "../$TS_FILE"
+    mv "../$M3U8_FILE.tmp" "../$M3U8_FILE"
     sleep 5
 done
