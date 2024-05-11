cf2ab5f00f74f09481b9254765b32d4f9f65ed64
Bilal Hassan <bilal.hassan7071@gmail.com>
2024-05-11 21:19:35 +0500

    Update ptv2.sh

--- ptv2.sh
+++ ptv2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STREAM_URL="http://starshare.live:8080/PTVSports/SportsPTV/61674"
-M3U8_FILE="live_channel.m3u8"
+M3U8_FILE="ptv.m3u8.tmp"  # Corrected file name
 TS_FILE="ptv.m3u8"
 
 # Trap Ctrl+C to gracefully exit the loop
@@ -10,7 +10,10 @@ trap 'echo "Exiting..."; exit' INT
 # Infinite loop for continuous execution
 while true; do
     rm -f "$TS_FILE"
-    ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "../$TS_FILE"
-    mv "../$M3U8_FILE.tmp" "../$M3U8_FILE"
+    if ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "../$M3U8_FILE"; then
+        mv "../$M3U8_FILE" "../$TS_FILE"
+    else
+        echo "Error: Stream ended prematurely or encountered an issue. Retrying in 5 seconds..."
+    fi
     sleep 5
 done
