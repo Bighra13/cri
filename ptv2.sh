c65401fc21a062e8e7f220584c1c753f3912dc30
Bilal Hassan <bilal.hassan7071@gmail.com>
2024-05-11 21:29:26 +0500

    Update ptv2.sh

--- ptv2.sh
+++ ptv2.sh
@@ -13,7 +13,7 @@ while true; do
     if ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "$M3U8_FILE"; then
         mv "$M3U8_FILE" "$TS_FILE"
     else
-        echo "Error: Stream ended prematurely or encountered an issue. Retrying in 5 seconds..."
+        echo "Error: Stream ended prematurely or encountered an issue. Retrying in 5 seconds...."
     fi
     sleep 5
 done
