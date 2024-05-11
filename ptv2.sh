6a2e60e2b8244914b8bc5b36524c6967a432fe4c
Bilal Hassan <bilal.hassan7071@gmail.com>
2024-05-11 21:37:04 +0500

    Update ptv2.sh

--- ptv2.sh
+++ ptv2.sh
@@ -11,7 +11,7 @@ trap 'echo "Exiting..."; exit' INT
 # Infinite loop for continuous execution
 while true; do
 rm -f "$TS_FILE"
-ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "$TS_FILE"
+ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "../$TS_FILE"
 #mv "$M3U8_FILE.tmp" "$M3U8_FILE"
     sleep 5
 done
