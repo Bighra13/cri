167db7e13fbd82176917e5361d78604ef3aa8c65
Bilal Hassan <bilal.hassan7071@gmail.com>
2024-05-11 22:02:53 +0500

    Update ptv2.sh

--- ptv2.sh
+++ ptv2.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 STREAM_URL="http://starshare.live:8080/PTVSports/SportsPTV/61674"
-M3U8_FILE="live_channel.m3u8"
-TS_FILE="ptv.m3u8"
+M3U8_FILE="ptv.m3u8"
+TS_FILE="ptv0.ts"
 
 # Trap Ctrl+C to gracefully exit the loop
 trap 'echo "Exiting..."; exit' INT
 
 # Function to commit and push changes
 commit_and_push() {
-    git config --local user.email "bilal.hassan7071@gmail.com"
-    git config --local user.name "Bighra13"
-    git add "$M3U8_FILE"
-    git commit -m "Update live channel file"
+    git add "$M3U8_FILE" "$TS_FILE"
+    git commit -m "Update live channel files"
     git push
 }
 
 # Infinite loop for continuous execution
 while true; do
     rm -f "$TS_FILE"
-    ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "$TS_FILE"
-    mv "$M3U8_FILE.tmp" "$M3U8_FILE"
+    ffmpeg -i "$STREAM_URL" -c copy -f hls -hls_time 30 -hls_list_size 2 -hls_flags delete_segments "$M3U8_FILE"
+    
+    # Check if the .m3u8 file exists before trying to move it
+    if [ -f "$M3U8_FILE.tmp" ]; then
+        mv "$M3U8_FILE.tmp" "$M3U8_FILE"
+    fi
     
     # Call the commit and push function
     commit_and_push
